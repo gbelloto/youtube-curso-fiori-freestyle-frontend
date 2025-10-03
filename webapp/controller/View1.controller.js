75d6d49dc28008308733f5b9c9869e9019f361e4
Gustavo Belloto Paganini <gustavo.belloto@gmail.com>
2025-10-03 18:18:42 -0300

    Aula data biding e model

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,48 +1,96 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/ui/core/mvc/Controller"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,MessageToast) {
+    function (Controller) {
         "use strict";
 
-        return Controller.extend("zov.controller.View1", {
-            onInit: function () {
-                //alert("onInit");
-            },
+        return Controller.extend("zov.controller.View1",{
+            onInit: function(){
+                
+                // model padrao da view
+                /*var oView = this.getView();
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({"usuario": {"nome": "Gustavo Belloto"}});
+                oView.setModel(oModel);*/
 
-            onBeforeRendering: function(){
-                //alert("onBeforeRendering");
-            },
 
-            onAfterRendering: function(){
-                //alert("onAfterRendering");
-            },
-
-            onExit: function(){
-                //alert("onExit");
-            },
-
-            onCalcular: function(){
+                //Data biding
                 var oView = this.getView();
-                var iB1 = parseInt(oView.byId("b1").getValue());
-                var iB2 = parseInt(oView.byId("b2").getValue());
-                var iB3 = parseInt(oView.byId("b3").getValue());
-                var iB4 = parseInt(oView.byId("b4").getValue());
-                var fResultado = 0;
-                
-                fResultado = ( iB1 + iB2 + iB3 + iB4) / 4;
 
-                oView.byId("resultado").setValue(fResultado);
+                var oModel1 = new sap.ui.model.json.JSONModel();
+                oModel1.setData({"usuario": {"nome": "Gustavo"}});
+                oModel1.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+                oView.setModel(oModel1, "oneway");
 
-                MessageToast.show("Resultado = "+fResultado);
+                var oModel2 = new sap.ui.model.json.JSONModel();
+                oModel2.setData({"usuario": {"nome": "Gustavo"}});
+                oModel2.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+                oView.setModel(oModel2, "twoway");
 
-                // MessageToast
-                // sap.m.MessageToast
-                
-                //oView.destroy();
+                var oModel3 = new sap.ui.model.json.JSONModel();
+                oModel3.setData({"usuario": {"nome": "Gustavo"}});
+                oModel3.setDefaultBindingMode(sap.ui.model.BindingMode.OneTime);
+                oView.setModel(oModel3, "onetime");
+
+                // model com o nome "dados"
+                var oView = this.getView();
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({"usuario": {"nome": "Carol Fuentes"}});
+                oView.setModel(oModel, "dados");
+            },
+
+            onTestOneWay: function(){
+                var oView = this.getView();
+
+                var oModel = oView.getModel("oneway");
+                var oData = oModel.getData();
+                oData.usuario.nome += ".";
+                oModel.setData(oData);
+            },
+            onTestTwoWay: function(){
+                var oView = this.getView();
+
+                var oModel = oView.getModel("twoway");
+                var oData = oModel.getData();
+                oData.usuario.nome += ".";
+                oModel.setData(oData);
+            },
+
+            onTestOneTime: function(){
+                var oView = this.getView();
+
+                var oModel = oView.getModel("onetime");
+                var oData = oModel.getData();
+                oData.usuario.nome += ".";
+                oModel.setData(oData);
+            },
+
+            onTestModels: function(){
+
+            // model usuarios
+            var oModel = this.getOwnerComponent().getModel("usuarios");
+            var oData = oModel.getData();
+            console.log("Model dos usuarios");
+            console.log(oData);
+
+            console.log("-------------------------------");
+
+            // model do servico
+            var oModel = this.getOwnerComponent().getModel();
+                oModel.read("/OVCabSet",{
+                    success: function(oData, oResponse){
+                        console.log("Dados retornados do servico");
+                        console.log(oData);
+                        console.log(oResponse);
+                    },
+                    error: function(oError){
+                        console.log(oError);    
+                    }
+                }
+            )
             }
         });
     });
