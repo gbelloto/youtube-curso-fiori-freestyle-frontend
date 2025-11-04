eb64db018cf57e00b80740604365c616cf9fd7ee
Gustavo Belloto Paganini <gustavo.belloto@gmail.com>
2025-11-04 10:59:12 -0300

    Aual de dialog

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,16 +8,33 @@ sap.ui.define([
     function (Controller, MessageToast, formatter) {
         "use strict";
 
-        return Controller.extend("zov_grp.controller.View1",{
+        return Controller.extend("zovgrp.controller.View1",{
 
            
             onInit: function(){
               
             },
 
-            onHeader: function(){
-                alert("onHeader");
+            onOpenDialogInfo: function(){
+                var that = this;
+                var sName = "zovgrp.view.DialogInfo";
+
+                if(!this.oDialog){
+                    this.loadFragment({
+                        name: sName
+                    }).then(function(oDialog2){
+                        that.oDialog = oDialog2;
+                        that.oDialog.open();
+                    }.bind(this));
+            }else{
+                this.oDialog.open();
             }
 
+        },
+
+        onCloseDialogInfo: function(){
+            this.byId("DialogInfo").close();
+        }
+    
         });
     });
